44bb3342672558b430c690a9df91cf9523f6cc18
smnich <stanislaw.mnich02@gmail.com>
2025-03-11 09:41:47 +0100

    Dodanie pętli czekającej na input użytkownika, Wyświetlanie argumentów użytych do funkcji, Dodanie lini oddzielających poszczególne funkcje

--- ZIG01/Program.cs
+++ ZIG01/Program.cs
@@ -9,31 +9,63 @@ public class Zadanie01
 
         try
         {
-            Console.WriteLine(myAplikacja.potega(2, 2, 2));
-
-            tablica = myAplikacja.zapiszWTablicy(tablica, 2);
-            for (int i = 0; i < tablica.Length; i++)
-            {
-                Console.Write(tablica[i] + " ");
-            }
-            Console.WriteLine("");
-            
-            Console.WriteLine(myAplikacja.poleKola(2));
-
-            Console.WriteLine(myAplikacja.sumaCyfr(123));
-
-            tablica = myAplikacja.zamienElementy(tablica, 1, 2, 1);
-            for (int i = 0; i < tablica.Length; i++)
-            {
-                Console.Write(tablica[i] + " ");
-            }
-
+            Console.WriteLine("Argumenty funkcji potega: 2, 2, 2");
+            Console.WriteLine("potega: " + myAplikacja.potega(2, 2, 2));
         }
-        catch (Exception e)
+        catch (WarningException e)
         {
             Console.WriteLine(e.Message);
         }
+        Console.WriteLine("=======================");
+        Console.WriteLine("Tablica przed zapiszWTablicy: ");
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i] + " ");
+        }
+        Console.WriteLine("");
+        tablica = myAplikacja.zapiszWTablicy(tablica, 2);
+        Console.WriteLine("Tablica po zapiszWTablicy(tablica, 2): ");
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i] + " ");
+        }
 
+        Console.WriteLine("");
+       Console.WriteLine("=======================");
+
+        var r = 2;
+        Console.WriteLine("Pole koła dla promienia równego " + r + " : " + myAplikacja.poleKola(r));
+        Console.WriteLine("=======================");
+
+        var sumowanaLiczba = 123;
+        Console.WriteLine("Argument funkcji sumaCyfr: " + sumowanaLiczba);
+        Console.WriteLine("sumaCyfr: " + myAplikacja.sumaCyfr(sumowanaLiczba));
+        Console.WriteLine("=======================");
+
+        Console.WriteLine("Tablica przed zamienElementy(): ");
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i] + " ");
+        }
+        Console.WriteLine("");
+
+        tablica = myAplikacja.zamienElementy(tablica, 1, 2, 1);
+        Console.WriteLine("Tablica po zamienElementy(tablica, 1, 2, 1): ");
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i] + " ");
+        }
+
+        while (true)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Wpisz 0, aby zakończyć program: ");
+            var end = int.Parse(Console.ReadLine());
+            if (end == 0)
+            {
+                break;
+            }
+        }
     }
 
     public int potega(int a, int b, int c)
